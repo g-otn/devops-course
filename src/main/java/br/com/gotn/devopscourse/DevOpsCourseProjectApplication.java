2b83efd54f33733fc900746adeb70a142e360b91
g-otn <g_otn@hotmail.com>
2020-07-06 09:41:38 -0300

    create: initial project files

---
+++ src/main/java/br/com/gotn/devopscourse/DevOpsCourseProjectApplication.java
@@ -0,0 +1,13 @@
+package br.com.gotn.devopscourse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevOpsCourseProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevOpsCourseProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/gotn/devopscourse/DevOpsCourseProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.gotn.devopscourse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevOpsCourseProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
